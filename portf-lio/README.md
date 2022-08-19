7a67e0c447936d1023c800677fe767d5f8b9db07
Eduardo Viana <91719993+Nekroz-Tech@users.noreply.github.com>
2022-08-19 15:49:33 -0300

    Delete portf-lio directory

--- portf-lio/README.md
+++
@@ -1,4 +0,0 @@
-# Portfólio
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/Viana00/pen/rNwRdMm](https://codepen.io/Viana00/pen/rNwRdMm).
-
--- portf-lio/src/script.js
+++
@@ -1,8 +0,0 @@
-function mudaTema() {
-  document.body.classList.toggle("light")
-}
-
-//add certificados
-//add circuitos de arduino
-//mudar para foto cyberpunk
-// add página da MOB
